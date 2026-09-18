72fc9537d9827d1952be5eef9e62b9ec5aa29f36
alpenglow93 <dktmzptm@gmail.com>
2026-09-18 15:50:19 +0900

    260918

---
+++ src/main/java/com/sist/web/mapper/FoodMapper.java
@@ -0,0 +1,38 @@
+package com.sist.web.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import com.sist.web.vo.FoodVO;
+
+import java.util.*;
+
+@Mapper
+@Repository
+public interface FoodMapper {
+	/*
+	 * <select id="foodListData" resultType="com.sist.web.vo.FoodVO" parameterType="hashmap">
+			SELECT no,poster,name,score,theme,type
+			FROM food
+			
+			<if test="search!=null and search!=''">
+				WHERE address LIKE CONCAT('%',#{search},'%')
+			</if>
+			
+			ORDER BY no ASC
+			OFFSET #{start} ROWS FETCH NEXT 12 ROWS ONLY
+		</select>
+	*/
+	public List<FoodVO> foodListData(Map map);
+	
+	/*
+		<select id="foodListTotalPage" resultType="int" parameterType="string">
+			SELECT count(*)
+			FROM food
+			<if test="search!=null and search!=''">
+				WHERE address LIKE CONCAT('%',#{search},'%')
+			</if>
+		</select>
+	 */
+	public int foodListTotalPage(String search);
+}
